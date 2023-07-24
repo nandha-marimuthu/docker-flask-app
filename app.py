f3efb79079b9b6625d3c51584d263951a6b03d2d
Nandhabalan Marimuthu <67896802+nandha-marimuthu@users.noreply.github.com>
2023-07-24 15:25:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Welcome to Prodapt Solutions HQ'
+    return 'Welcome to Prodapt Solutions HQ Guindy'
 
 
 if __name__ == "__main__":
